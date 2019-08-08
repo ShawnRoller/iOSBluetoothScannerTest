a448715572517b09c7fbd25206b8fa69b7881e66
Offensivelybad <offensivelybad@yahoo.com>
2019-08-08 12:23:13 -0400

    Initial Commit

---
+++ iOSBluetoothScannerTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOSBluetoothScannerTest
+//
+//  Created by Evolve Dev on 8/8/19.
+//  Copyright © 2019 Evolve Dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
